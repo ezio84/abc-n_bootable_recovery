949f7428ca338ff0f71fbaf3df27794e78ea7a6b
Joe Schubert <joseph.schubert@gmail.com>
2017-08-23 23:54:13 +0200

    Update-binary: stop calling log commands that do not exist in TWRP

--- updater/blockimg.cpp
+++ updater/blockimg.cpp
@@ -1794,12 +1794,6 @@ pbiudone:
       LOG(INFO) << "stashed " << params.stashed << " blocks";
       LOG(INFO) << "max alloc needed was " << params.buffer.size();
 
-      const char* partition = strrchr(blockdev_filename->data.c_str(), '/');
-      if (partition != nullptr && *(partition + 1) != 0) {
-        fprintf(cmd_pipe, "log bytes_written_%s: %zu\n", partition + 1, params.written * BLOCKSIZE);
-        fprintf(cmd_pipe, "log bytes_stashed_%s: %zu\n", partition + 1, params.stashed * BLOCKSIZE);
-        fflush(cmd_pipe);
-      }
       // Delete stash only after successfully completing the update, as it may contain blocks needed
       // to complete the update later.
       DeleteStash(params.stashbase);
